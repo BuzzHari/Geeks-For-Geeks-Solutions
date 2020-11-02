b16e587c34683ecc6b22707e8f28e55ec7b92ca8
Hari <buzz.hari@gmail.com>
2020-11-03 00:55:17 +0530

    solved longest common substring

---
+++ Algorithms/Dynamic_Programming/Longest-common-substring.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+
+    int t;
+    cin >> t;
+    int n, m;
+    string str1, str2; 
+    while(t--) {
+        cin >> n >> m;
+        
+        cin >> str1 >> str2; 
+        
+        // lcs[i][j] = length of the longest common substring in str1(0...i-1) and str2(0..j-1).
+        int lcs[str1.size()+1][str2.size()+1];
+        // To keep track of the longest substring. 
+        int ans=0;
+        for (int i = 0; i <= str1.size(); ++i) {
+            for (int j = 0; j <= str2.size(); ++j) {
+
+                if (i == 0 || j == 0)
+                    lcs[i][j] = 0;
+                
+                // If matching characters at the same position, then increment the length. 
+                else if (str1[i-1] ==  str2[j-1]) {
+                    lcs[i][j] = 1 + lcs[i-1][j-1];
+                    ans = max(ans, lcs[i][j]);
+                }
+                else 
+                    lcs[i][j] = 0;
+
+            }
+        }
+        cout << ans << "\n"; 
+    }
+
+}
